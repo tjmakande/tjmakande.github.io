71a6a48dc42058d0b474ac9c037abdcbadab7a10
TJ <tjmakande@gmail.com>
2022-02-06 17:09:37 +0800

    design reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,17 @@
-import React, {Suspense, useState} from 'react';
-import styled from 'styled-components';
+import React from 'react';
 
 import './App.css';
 
-import { Canvas } from '@react-three/fiber';
-// import { OrbitControls } from '@react-three/drei'
-
-import {Pages} from './utils/Pages'
-
-
-import Model from './Components/Model';
 import LandingPage from './Sections/LandingPage';
 import AboutMe from './Sections/AboutMe';
 import WhatIDo from './Sections/WhatIDo';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState<string>('Home');
   return (
     <div className="App">
-      <Canvas style={{position: 'fixed', width: '100vw', height: '100vh', zIndex: 0}}>
-        <Suspense fallback={<>loading</>}>
-        <directionalLight position={[0, 10, 5]} intensity={0.7} />
-        <directionalLight position={[-10, -10, -5]} intensity={0.2} />
-          <Model page={currentPage}/>
-          {/* <OrbitControls /> */}
-        </Suspense>
-      </Canvas>
-      <Page>
-        <LandingPage />
-        <AboutMe />
-        <WhatIDo page={(active) => active ? setCurrentPage(Pages[2]) : setCurrentPage('Home')} />
-      </Page>
-
+      <LandingPage />
     </div>
   );
 }
 
 export default App;
-
-
-const Page = styled.div`
-  background-color: #00000021;
-  z-index: 2;
-  padding: 0 2vw;
-  display: flex;
-  flex-direction: column;
-`;
--- src/utils/Pages.ts
+++
@@ -1,1 +0,0 @@
-  export const Pages = ['Home', 'AboutMe', 'WhatIDo'];
