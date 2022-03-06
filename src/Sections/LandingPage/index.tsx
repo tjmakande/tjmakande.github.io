30504b18e23ad65e70ad1e39558e776c379d38b3
TJ <tjmakande@gmail.com>
2022-03-06 12:03:13 +0800

    add cool new Landing section Text for name

--- src/Sections/LandingPage/index.tsx
+++ src/Sections/LandingPage/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import Name from 'Assets/Name.gif';
 import SectionWrapper from '../../Components/Styled/SectionWrapper';
 
 const LandingPage = React.forwardRef<HTMLDivElement>((props, ref)=> {
@@ -9,9 +10,11 @@ const LandingPage = React.forwardRef<HTMLDivElement>((props, ref)=> {
         <SectionWrapper ref={ref}>
             <>
                 <Wrapper>
-                    <Header id="Name">
+                    {/* <Header id="Name">
                         TAPIWANASHE J. MAKANDE
-                    </Header> 
+                    </Header>  */}
+
+                    <NameItem src={Name} />
                 </Wrapper>
             </>
         </SectionWrapper>
@@ -20,12 +23,19 @@ const LandingPage = React.forwardRef<HTMLDivElement>((props, ref)=> {
 
 export default LandingPage;
 
+const NameItem = styled.img`
+    width: 100%;
+    position: absolute;
+    margin: 0 auto;
+    transform: scale(1);
+`;
 const Wrapper = styled.div`
     height: 130vh;
     width: 100vw;
     display: flex;
     justify-content: center;
     align-items: center;
+    background-color: rgb(229, 229, 229);
 `;
 
 const Header = styled.h1`
