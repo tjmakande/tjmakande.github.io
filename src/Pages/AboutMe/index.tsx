b931750f2c1cdfdde7dbd73f8d52915ed3e9287d
TJ <tjmakande@gmail.com>
2022-03-08 16:47:28 +0800

    Works page -- intro text move left

--- src/Pages/AboutMe/index.tsx
+++ src/Pages/AboutMe/index.tsx
@@ -2,6 +2,7 @@ import React, {createRef} from 'react';
 import styled from 'styled-components';
 import SectionWrapper from 'Components/Styled/SectionWrapper';
 import {Scrollbar} from 'smooth-scrollbar-react';
+import Footer from 'Sections/Footer';
 
 import TJImage from 'Assets/TJ_image.jpg';
 import TechImg from 'Assets/TechImg.jpeg';
@@ -19,26 +20,38 @@ const AboutMePage = () => {
     const IntroWrap = createRef<HTMLDivElement>();
     const Descriptionref = createRef<HTMLParagraphElement>();
 
+    const Zimref = createRef<HTMLSpanElement>();
+    const Beijingref = createRef<HTMLSpanElement>();
+
     const HeadMakeOpaque = (x: number) => x/400 < 1 ? 1 - (x/400) : 0;
+    const MoveTextLeft = (x: number) => x < window.innerWidth * 0.30 ? x : window.innerWidth * 0.30; 
+
+
 
     const scrollAnimation = (scrollY: number) => {
-
+        // animation part 1
         if(scrollY < window.innerHeight * 0.8){
              IntroWrap.current && (IntroWrap.current.style.visibility = 'hidden');
         } else { 
             IntroWrap.current && (IntroWrap.current.style.visibility = `visible`);
         }
 
+        // Animation part 2
         if(scrollY > window.innerHeight * 0.8){
             if(IntroWrap.current){
                 const instanceOffset = scrollY - window.innerHeight * 0.8;
                 Descriptionref.current && (Descriptionref.current.style.opacity =  `${instanceOffset / 200}`);
                 IntroWrap.current.style.transform = `translateY(${scrollY - window.innerHeight}px)`;
+
+                if( scrollY > window.innerHeight * 1.1){
+                    IntroWrap.current.style.transform = `translate3d(-${MoveTextLeft(scrollY - window.innerHeight * 1.1)}px, ${scrollY - window.innerHeight}px, 0)`;
+                    // console.log(MoveTextLeft(scrollY-window.innerHeight * 1.1))
+                }
             }
         }
 
-        if(scrollY > window.innerHeight * 1.5) {
-            Descriptionref.current && (Descriptionref.current.style.opacity = `${HeadMakeOpaque(scrollY - window.innerHeight * 1.5)}`);
+        if(scrollY > window.innerHeight * 2) {
+            Descriptionref.current && (Descriptionref.current.style.opacity = `${HeadMakeOpaque(scrollY - window.innerHeight * 2)}`);
         }
     }
 
@@ -53,14 +66,33 @@ const AboutMePage = () => {
                         </Header>
                     </div>
                 </HeaderWrapper>
-                <TimeContainer>
+                
+                <IntroContainer>
                     <IntroWrapper ref={IntroWrap}>
                         <Description ref={Descriptionref}>
-                            I am a web developer, from Zimbabwe, currently based in Beijing
+                            I am a web developer, from <Wordspan ref={Zimref}>Zimbabwe</Wordspan>, currently based in <Wordspan ref={Beijingref}>Beijing</Wordspan>.
                         </Description>
                     </IntroWrapper>
-                </TimeContainer>
+                </IntroContainer>
 
+
+                <IntroTwoContainer>
+                    <IntroTwoWrapper>
+                        <Description>
+                            I'm naturally curious, that's probably how I ended up learning MERN stack.
+                        </Description>
+                    </IntroTwoWrapper>
+                </IntroTwoContainer>
+
+                <IntroThreeContainer>
+                    <IntroThreeWrapper>
+                        <Description>
+                            I don't shy away from challenges, and I'm always looking forward to help you tackle yours!
+                        </Description>
+                    </IntroThreeWrapper>
+                </IntroThreeContainer>
+
+                <Footer />
             </SectionWrapper>
         </Scrollbar>
 
@@ -68,9 +100,48 @@ const AboutMePage = () => {
 }
 
 export default AboutMePage;
+const Wordspan = styled.span`
+    // opacity: 0;
+`;
+
+const IntroContainer = styled.div`
+    width: 100%;
+    height: 400vh;
+    position: relative;
+    background-color: white;
+    display: flex;
+    flex-direction: row;
+`;
+
+const IntroWrapper = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const IntroTwoContainer = styled(IntroContainer)`
+    height: 150vh;
+`;
+
+const IntroTwoWrapper = styled(IntroWrapper)`
+    justify-content: start;
+    padding: 0 10vw;
+`;
+
+const IntroThreeContainer = styled(IntroContainer)`
+    height: 150vh;
+`;
+
+const IntroThreeWrapper = styled(IntroWrapper)`
+    justify-content: start;
+    padding: 0 10vw;
+`;
+
 
 const Description = styled.p`
-    opacity: 0;
     font-size: clamp(2rem,2vw, 5rem);
     width: 30vw;
 `;
@@ -81,22 +152,9 @@ const LandingImg = styled.img`
     left: 0;
 `;
 
-const IntroWrapper = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`;
 
-const TimeContainer = styled.div`
-    width: 100%;
-    height: 200vh;
-    position: relative;
-    background-color: white;
-    display: flex;
-    flex-direction: row;
-`;
+
+
 
 const HeaderWrapper = styled.div`
     background-color: rgb(229, 229, 229);
