d76000c69093e036fc4bb90af1ee6f8a3a2d4543
TJ <tjmakande@gmail.com>
2022-03-20 15:49:42 +0800

    Add text to the about me section in the home page

--- src/Sections/AboutMe/index.tsx
+++ src/Sections/AboutMe/index.tsx
@@ -5,21 +5,26 @@ const AboutMe = React.forwardRef<HTMLDivElement>((props, ref) => {
    
     return (
             <Wrapper ref={ref}>
-               
+               <Text> A web developer who diligently works to find the best solution to any given challenge, drawing the ideas created in your head, on to the canvas of reality.</Text>
             </Wrapper>
     )
 });
 
 export default AboutMe;
 
+const Text = styled.p`
+    color: black;
+    width: 60%;
+    margin: 0 auto;
+    font-size: clamp(2rem, 2.5vw, 5rem);
+
+`;
+
 const Wrapper = styled.div`
     height: 100vh;
     width: 100vw;
-    z-index: 4;
     display: flex;
     flex-direction: column;
-    color: black;
-    // padding: 0 5vw 0 0;
-    position: relative;
     overflow: hidden;
+    justify-content: center;
 `;
