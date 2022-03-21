70773ba23fdf0a07fbb8d95eb3ac05596221d3b4
TJ <tjmakande@gmail.com>
2022-03-21 12:03:59 +0800

    Attempt with scroll trigger

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -7,6 +7,31 @@ import Works from 'Sections/Works';
 import Footer from 'Sections/Footer';
 import {Scrollbar} from 'smooth-scrollbar-react';
 import { isVoidExpression } from 'typescript';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
+
+// ScrollTrigger.scrollerProxy('.scroller', {
+//   scrollTop(value) {
+//     if (arguments.length) {
+//       bodyScrollBar.scrollTop = value; // setter
+//     }
+//     return bodyScrollBar.scrollTop;    // getter
+//   },
+//   getBoundingClientRect() {
+//     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+//   }
+// });
+
+gsap.to(".solution", {
+  scrollTrigger: {
+      trigger: ".wrapper",
+      start: "top top",
+      end: '+=100',
+      scrub: 1,
+  },
+  scale: 5,
+});
 
 const HomePage = () => {
     const LandingSectionref = React.createRef<HTMLDivElement>();
@@ -21,35 +46,91 @@ const HomePage = () => {
     return(
         <Scrollbar 
         continuousScrolling={false}
-        onScroll={(status) => {
-          const scrollY = status.offset.y
+        onScroll={(status, Scrollbar) => {
+          const scrollY = status.offset.y;
+
+          if(Scrollbar){
+            Scrollbar.addListener(ScrollTrigger.update);
+            ScrollTrigger.scrollerProxy('.scroller', {
+              scrollTop() {
+                if (arguments.length) {
+                  Scrollbar.scrollTop = 0; // setter
+                }
+                return Scrollbar.scrollTop;    // getter
+              },
+              getBoundingClientRect() {
+                return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+              }
+            });
+          }
+
+
+          const t1 = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".wrappers",
+                start: "top top",
+                end: 'center top',
+                scrub: .5,
+            }
+        });
+
+        t1.to(".solution", {scale: 5, duration: 2});
           //page background color
            Backgroundref.current && (Backgroundref.current.style.transform = `translateY(${scrollY}px)`);
           //  SectionWrapperref.current && (SectionWrapperref.current.style.visibility = scrollY > window.innerHeight * 0.5 ? 'hidden' : 'visible');
 
           if(LandingSectionref.current){
-            const div = LandingSectionref.current;
-            const Logo = div.children[0].children[0] as HTMLImageElement;
+            const Landing = LandingSectionref.current;
+            const Logo = Landing.children[0].children[0] as HTMLImageElement;
             Logo.style.transform =`translateY(${scrollY}px)`;
-            Logo.style.width = `${100 - scrollY*0.5 > 10 ? 100 - scrollY*0.5 : 10}%`;
+            Logo.style.width = `${100 - scrollY*0.5 > 15 ? 100 - scrollY*0.5 : 15}%`;
           }
 
           //About Me Section
           if(AboutMeContainerref.current){
-          
+            // const Aboutme = AboutMeContainerref.current;
+            // const Text = Aboutme.children[0] as HTMLDivElement;
+            // const HideText1 = Text.children[0] as HTMLSpanElement;
+            // const Solutionwrd = Text.children[1] as HTMLParagraphElement;
+            // const Towrd = Text.children[2] as HTMLParagraphElement;
+            // const HideText2 =  Text.children[3] as HTMLSpanElement;
+            // const Challengewrd = Text.children[4] as HTMLParagraphElement;
+            // const HideText3 = Text.children[5] as HTMLSpanElement;
+
+            // const opacitycalc = 1 - ((scrollY - window.innerHeight * 1.1)/100);
+            // const opacity = opacitycalc < 0 ? 0 : opacitycalc
+            // const scale = ((scrollY - window.innerHeight)/100) * 1.15;
+            // const translationBase = scrollY - window.innerHeight * 1.1;
+
+            // if(scrollY > window.innerHeight && scrollY < window.innerHeight * 2){
+            //   Aboutme.style.transform = `translateY(${scrollY - window.innerHeight}px)`;
+
+            //   if(scrollY > window.innerHeight * 1.1){
+            //     HideText1.style.opacity = `${opacity}`;
+            //     HideText2.style.opacity = `${opacity}`;
+            //     HideText3.style.opacity = `${opacity}`;
+
+            //     console.log(scale);
+
+            //     Solutionwrd.style.transform = `scale(${scale < 1 ? 1 : scale}, ${scale < 1 ? 1 : scale})`
+            //     Towrd.style.transform = `scale(${scale < 1 ? 1 : scale}, ${scale < 1 ? 1 : scale})`
+            //     Challengewrd.style.transform = `scale(${scale < 1 ? 1 : scale}, ${scale < 1 ? 1 : scale}) translate3d(${translationBase}px, ${-translationBase}px, 0)`
+            //   }
+
+            // }
           }
 
           //Work section
           if(Worksref.current){
-            if(scrollY > window.innerHeight * 2.3 && scrollY < window.innerHeight * 2.7){
-              Worksref.current && (Worksref.current.style.transform = `translateY(${scrollY - window.innerHeight * 2.3}px)`)
-            }else if(scrollY > window.innerHeight * 2.7) {
-              Worksref.current && (Worksref.current.style.transform = `translateY(${(window.innerHeight * 2.7) - window.innerHeight * 2.3}px)`)
+            if(scrollY > window.innerHeight * 3 && scrollY < window.innerHeight * 3.7){
+              Worksref.current && (Worksref.current.style.transform = `translateY(${scrollY - window.innerHeight * 3}px)`)
+            }else if(scrollY > window.innerHeight * 3.7) {
+              Worksref.current && (Worksref.current.style.transform = `translateY(${(window.innerHeight * 3.7) - window.innerHeight * 3}px)`)
             }
           }
 
-          if(scrollY > window.innerHeight * 3.3){
-            const y = 1 + (scrollY - window.innerHeight * 3.3) / 150;
+          if(scrollY > window.innerHeight * 4.3){
+            const y = 1 + (scrollY - window.innerHeight * 4.3) / 150;
             const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
             
             Backgroundref.current && (Backgroundref.current.style.backgroundColor = `rgb(${r < 58 ? 58 : r}, ${g < 58 ? 58 : g}, ${b < 58 ? 58 : b} )`);
--- src/Sections/AboutMe/index.tsx
+++ src/Sections/AboutMe/index.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import styled from 'styled-components';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger)
 
 const AboutMe = React.forwardRef<HTMLDivElement>((props, ref) => {
+    // const TextWrapperref = useRef<HTMLDivElement>();
+
+    // const t1 = gsap.timeline({
+    //     scrollTrigger:{
+    //         trigger: TextWrapperref.current,
+    //         start: "center center",
+    //         end: "bottom top",
+    //         scrub: true, 
+    //         markers: true
+    //     }
+    // });
+
+    
    
     return (
-            <Wrapper ref={ref}>
-               <Text> A web developer who diligently works to find the best solution to any given challenge, drawing the ideas created in your head, on to the canvas of reality.</Text>
+            <Wrapper className='wrappers' ref={ref}>
+                <Text className='TextDiv'> 
+                    <span>A web developer who diligently works to find the best </span>
+                    <span className={'solution'} style={{display: 'inline-block'}}>solution </span>
+                    <span style={{display: 'inline-block'}}>to </span>
+                    <span>any given </span>  
+                    <span style={{display: 'inline-block'}}>challenge</span>
+                    <span>, drawing the ideas created in your head, on to the canvas of reality.</span> 
+                </Text>
             </Wrapper>
     )
 });
 
 export default AboutMe;
 
-const Text = styled.p`
-    color: black;
+const Text = styled.div`
     width: 60%;
     margin: 0 auto;
+    color: black;
     font-size: clamp(2rem, 2.5vw, 5rem);
-
 `;
 
 const Wrapper = styled.div`
@@ -27,4 +49,5 @@ const Wrapper = styled.div`
     flex-direction: column;
     overflow: hidden;
     justify-content: center;
+    margin-bottom: 100vh;
 `;
